5bd8cd19f768721bbbefd98bf2744bea0a06e5e7
togami2864 <togami2864@mercari.com>
2024-06-22 15:19:55 +0900

    chore: ga

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
 import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), expressiveCode(), mdx()],
+  integrations: [
+    react(),
+    expressiveCode(),
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   prefetch: true,
 });
